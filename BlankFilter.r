2296b735228aa819b4bd89f3c6fb8f7814d535f5
stephanieherman <stephanie.herman@live.se>
2016-01-15 15:14:06 +0100

    Functional
    does not output the result tho

--- BlankFilter.r
+++ BlankFilter.r
@@ -42,28 +42,15 @@ removegrep <- function(x,name) {
 #names(options.args) <- unlist(options.names)
 #print(options.args)
 
-samples<-read.table("~/data/inputfile.xls",sep='\t',header=T)
-print(names(samples))
-#samples=removegrep(samples,"^intensity_BLANK_B1_Rep1$")
-#samples=removegrep(samples,"^intensity_BLANK_B1_Rep1_2$")
-#samples=removegrep(samples,"Pool")
-#samples=removegrep(samples,"After")
-#samples=removegrep(samples,"Daidzein")
-#samples=removegrep(samples,"^intensity_Mebendazole_B1_Rep1$")
-#samples=removegrep(samples,"^intensity_Mebendazole_B2_Rep2$")
-#samples=removegrep(samples,"^intensity_Mebendazole_B2_Rep2_2$")
-#samples=removegrep(samples,"^intensity_Prednisolone_B2_Rep2$")
-#samples=removegrep(samples,"^intensity_Wortmannin_B4_Rep2$")
+samples<-read.table("inputfile.xls",sep='\t',header=T)
+#print(names(samples))
 
 # separate blanks
-#Blanks=specgrep(samples,"BLANK")
-#samples=removegrep(samples,"BLANK")
-#Blanks<-samples[,grep("BLANK",names(samples))]
-#samples<-samples[,-grep("BLANK",names(samples))]
-#cat("sample dim: ", dim(samples), "\n")
-#cat("Blanks dim: ", dim(Blanks), "\n")
-#to.remove<-AdvancedBlankFilter(Blanks,samples,0.01)
-#samples=samples[-to.remove,]
-#cat("GJ", "\n")
+Blanks=specgrep(samples,"BLANK")
+samples=removegrep(samples,"BLANK")
+
+to.remove<-AdvancedBlankFilter(Blanks,samples,0.01)
+samples=samples[-to.remove,]
+cat("GJ", "\n")
 
 #write.table(samples,file=args,sep='\t',row.names=F)
--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,5 @@ RUN apt-get update && apt-get install --yes texlive-binaries
 RUN echo "deb http://ftp.acc.umu.se/mirror/CRAN/bin/linux/ubuntu trusty/" >> /etc/apt/sources.list
 RUN apt-get update --yes && apt-get install --yes --force-yes r-base
 
-RUN mkdir -p /data 
-WORKDIR /data
-VOLUME /data
-
-ADD BlankFilter.r ./
+ADD BlankFilter.r /
 ENTRYPOINT ["Rscript", "BlankFilter.r"] 
