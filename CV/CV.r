c8c6309ed036f50ef369290cfe415baff1dab333
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2016-02-24 10:25:49 +0100

    remove third argument

--- CV/CV.r
+++ CV/CV.r
@@ -2,9 +2,8 @@ args <- commandArgs(trailingOnly = TRUE)
 
 input = args[1]
 output = args[2]
-folder = args[3]
 
-x<-read.table(paste(folder,input,sep="/"),sep='\t',header=T)
+x<-read.table(input,sep='\t',header=T)
 
 calc.cv <- function(x) {
   c=abs(sd(as.numeric(x))/mean(as.numeric(x)))
@@ -13,9 +12,5 @@ calc.cv <- function(x) {
 
 cv <- apply(x, 1, calc.cv) 
 
-ifelse(!dir.exists(paste(folder, output, sep="/")), dir.create(paste(folder, output, sep="/")), FALSE)
-
-out = paste(paste(folder, output, sep="/"), input, sep="/")
-
-write.table(cv,file=out,sep='\t',row.names=F)
+write.table(cv,file=output,sep='\t',row.names=F)
 
