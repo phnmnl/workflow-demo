18a4d675b2675bdd5729bc579d4b4f86c6d5c6f4
Anders <anders@ormbunkar.se>
2016-02-26 08:27:19 +0100

    frontweb edits

--- frontweb/html/header.php
+++ frontweb/html/header.php
@@ -1,11 +1,13 @@
 <?php
-    error_reporting(E_ALL);
+    // error_reporting(E_ALL);
+    // error_reporting(0);
+    error_reporting(E_ERROR | E_WARNING | E_PARSE);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="ISO-8859-1">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
